fc60ad752ce6ea7ccdd3d3e9d3c5ec6f576fc0fd
Wonhyuk Cho <zzangzwh@hotmail.com>
2023-11-18 14:39:51 -0700

    Add project files.

---
+++ WEB-API-Result/Domain/GetNorthwindCustomers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEB_API_Result.Domain
+{
+    internal class GetNorthwindCustomers
+    {
+        public string CustomerID { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        public string? ContactName { get; set; } = string.Empty;
+        public string? ContactTitle { get; set; } = string.Empty;
+        public string? Address { get; set; } = string.Empty;
+        public string? City { get; set; } = string.Empty;
+        public string? Region { get; set; } = string.Empty;
+        public string? PostalCode { get; set; } = string.Empty;
+        public string? Country { get; set; } = string.Empty;
+        public string? Phone { get; set; } = string.Empty;
+        public string? Fax { get; set; } = string.Empty;
+    }
+}
---
+++ WEB-API-Result/Program.cs
@@ -0,0 +1,82 @@
+﻿using WEB_API_Result.Domain;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace WEB_API_Result
+{
+    internal class Program
+    {
+        public static string BaseURL = @"https://localhost:7014";
+        static async Task Main(string[] args)
+        {
+            GetNorthwindCustomers getCustomers = new GetNorthwindCustomers();
+            Console.WriteLine("-----------");
+            Console.WriteLine("[HttpGet] async Task GetNorthwindCustomers()");
+
+            Console.WriteLine("-----------");
+            await GetNorthwindCustomers();
+            string customerID = "ALFKI";
+            Console.WriteLine("-----------");
+            Console.WriteLine("[HttpGet] async Task GetNorthwindCustomer()");
+
+            Console.WriteLine("-----------");
+            await GetNorthWindCustomer(customerID);
+        }
+        public static async Task GetNorthwindCustomers()
+        {
+            using (HttpClient httpApiClient = new HttpClient())
+            {
+                MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                httpApiClient.DefaultRequestHeaders.Accept.Add(contentType);
+                httpApiClient.BaseAddress = new Uri(BaseURL);
+
+                HttpResponseMessage responseMessage = new HttpResponseMessage();
+                string getWebApiContent = string.Empty;
+
+                //let properties able to camelcase or pascal case
+                JsonSerializerOptions options = new JsonSerializerOptions();
+                options.PropertyNameCaseInsensitive = true;
+
+                responseMessage = await httpApiClient.GetAsync("/api/GetCustomers");
+                getWebApiContent = await responseMessage.Content.ReadAsStringAsync();
+
+                List<GetNorthwindCustomers> datas = JsonSerializer.Deserialize<List<GetNorthwindCustomers>>(getWebApiContent, options);
+           
+
+                foreach(var data in datas)
+                {
+                    Console.WriteLine($"{data.CustomerID}\t{data.CompanyName}\t{data.ContactName}\t{data.ContactTitle}\t{data.Address}\t{data.City}\t {data.Region} \t{data.PostalCode}\t {data.Country}\t{data.Phone}\t{data.Fax}");
+                }
+
+
+            }
+        }
+        public static async Task GetNorthWindCustomer(string customerID)
+        {
+            using (HttpClient webApiClient = new HttpClient())
+            {
+                webApiClient.BaseAddress = new Uri(BaseURL);
+                webApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                JsonSerializerOptions options = new JsonSerializerOptions();
+                options.PropertyNameCaseInsensitive = true;
+                HttpResponseMessage responeMessage = await webApiClient.GetAsync($"/api/GetCustomers/{customerID}");
+
+                if (responeMessage.IsSuccessStatusCode)
+                {
+                    GetNorthwindCustomers data = new GetNorthwindCustomers();
+                    string getWebApiContent = await responeMessage.Content.ReadAsStringAsync();
+                    data = JsonSerializer.Deserialize<GetNorthwindCustomers>(getWebApiContent, options);
+                    Console.WriteLine($"{data.CustomerID}\t{data.CompanyName}\t{data.ContactName}\t{data.ContactTitle}\t{data.Address}\t{data.City}\t {data.Region} \t{data.PostalCode}\t {data.Country}\t{data.Phone}\t{data.Fax}");
+                }
+                else
+                {
+                    Console.WriteLine("API Request FAILED!");
+                }
+
+            }
+        }
+    }
+}
